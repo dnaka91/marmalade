83ed3daa3b48b2b0872ef8f85651402126fe707d
Dominik Nakamura <dnaka91@gmail.com>
2021-09-21 20:13:00 +0900

    Split repositories module into separate files

--- src/repositories/repo.rs
+++ src/repositories/repo.rs
@@ -1,183 +1,16 @@
-use std::{collections::HashSet, convert::TryFrom, io::ErrorKind, sync::Arc};
+use std::sync::Arc;
 
 use anyhow::{Context, Result};
-use argon2::{
-    password_hash::{rand_core::OsRng, SaltString},
-    Argon2, PasswordHash, PasswordHasher, PasswordVerifier,
-};
 use camino::{Utf8Path, Utf8PathBuf};
 use futures_util::FutureExt;
 use git2::{ErrorCode, ObjectType, Repository, Tree};
 use tokio::fs;
-use uuid::Uuid;
 
+use super::UserRepository;
 use crate::{
     dirs::DIRS,
-    models::{FileKind, RepoFile, UserAccount, UserRepo},
+    models::{FileKind, RepoFile, UserRepo},
 };
-
-pub struct UserRepository<'a> {
-    username: &'a str,
-    user_path: Utf8PathBuf,
-    user_file: Utf8PathBuf,
-}
-
-impl<'a> UserRepository<'a> {
-    pub fn for_user(username: &'a str) -> Self {
-        let user_path = DIRS.data_dir().join(username);
-        let user_file = user_path.join("user.json");
-
-        Self {
-            username,
-            user_path,
-            user_file,
-        }
-    }
-
-    pub fn repo<'b>(&self, name: &'b str) -> RepoRepository<'b> {
-        RepoRepository::from_user_repo(self, name)
-    }
-
-    pub async fn exists(&self) -> bool {
-        fs::metadata(&self.user_file).await.is_ok()
-    }
-
-    pub async fn visible(&self, auth_user: &str, user: &str) -> Result<bool> {
-        let info = self.load_info().await?;
-        Ok(!info.private || auth_user == user)
-    }
-
-    pub async fn create_user(&self, password: &str, private: bool, admin: bool) -> Result<bool> {
-        if self.exists().await {
-            return Ok(false);
-        }
-
-        let data = serde_json::to_vec_pretty(&UserAccount {
-            username: self.username.to_owned(),
-            password: hash_password(password)?,
-            private,
-            admin,
-        })?;
-
-        fs::create_dir_all(&self.user_path).await?;
-        fs::write(&self.user_file, data).await?;
-
-        Ok(true)
-    }
-
-    pub async fn is_valid_password(&self, password: &str) -> Result<bool> {
-        let user_file = fs::read(&self.user_file).await?;
-
-        let data = serde_json::from_slice::<UserAccount>(&user_file)?;
-
-        verify_password(password, &data.password)
-    }
-
-    pub async fn is_valid_token(&self, token: Uuid) -> Result<bool> {
-        let token_file = self.user_path.join("tokens.json");
-        let token_file = fs::read(token_file).await?;
-
-        let tokens = serde_json::from_slice::<HashSet<Uuid>>(&token_file)?;
-
-        Ok(tokens.contains(&token))
-    }
-
-    pub async fn add_token(&self, token: Uuid) -> Result<()> {
-        edit_tokens(self.username, |tokens| {
-            tokens.insert(token);
-        })
-        .await
-    }
-
-    pub async fn remove_token(&self, token: Uuid) -> Result<()> {
-        edit_tokens(self.username, |tokens| {
-            tokens.remove(&token);
-        })
-        .await
-    }
-
-    pub async fn list_user_names(&self, auth_user: &str) -> Result<Vec<String>> {
-        let mut entries = fs::read_dir(DIRS.data_dir()).await?;
-        let mut names = Vec::new();
-
-        while let Some(entry) = entries.next_entry().await? {
-            let path = Utf8PathBuf::try_from(entry.path())?;
-            let file_name = path.file_name().unwrap();
-
-            if auth_user != file_name
-                && fs::metadata(path.join("user.json")).await.is_ok()
-                && UserRepository::for_user(file_name)
-                    .visible(auth_user, file_name)
-                    .await?
-            {
-                names.push(file_name.to_owned());
-            }
-        }
-
-        Ok(names)
-    }
-
-    pub async fn list_repo_names(&self, auth_user: &str) -> Result<Vec<String>> {
-        let mut entries = fs::read_dir(&self.user_path).await?;
-        let mut names = Vec::new();
-
-        while let Some(entry) = entries.next_entry().await? {
-            let path = Utf8PathBuf::try_from(entry.path())?;
-
-            if fs::metadata(path.join("repo.json")).await.is_ok()
-                && fs::metadata(path.join("repo.git")).await.is_ok()
-                && RepoRepository::from_base(&self.user_path, path.file_name().unwrap())
-                    .visible(auth_user, self.username)
-                    .await?
-            {
-                names.push(path.file_name().unwrap().to_owned());
-            }
-        }
-
-        Ok(names)
-    }
-
-    async fn load_info(&self) -> Result<UserAccount> {
-        let data = fs::read(&self.user_file).await?;
-        serde_json::from_slice(&data).map_err(Into::into)
-    }
-}
-
-async fn edit_tokens(username: &str, edit: impl Fn(&mut HashSet<Uuid>)) -> Result<()> {
-    let user_path = DIRS.data_dir().join(username);
-    let real_file = user_path.join("tokens.json");
-    let temp_file = user_path.join("~tokens.json");
-
-    let mut tokens = match fs::read(&real_file).await {
-        Ok(buf) => serde_json::from_slice::<HashSet<Uuid>>(&buf)?,
-        Err(e) if e.kind() == ErrorKind::NotFound => HashSet::default(),
-        Err(e) => return Err(e.into()),
-    };
-
-    edit(&mut tokens);
-
-    let buf = serde_json::to_vec_pretty(&tokens)?;
-    fs::write(&temp_file, &buf).await?;
-    fs::rename(temp_file, real_file).await?;
-
-    Ok(())
-}
-
-fn hash_password(password: &str) -> Result<String> {
-    let salt = SaltString::generate(&mut OsRng);
-    let hasher = Argon2::default();
-
-    Ok(hasher
-        .hash_password(password.as_bytes(), &salt)?
-        .to_string())
-}
-
-fn verify_password(password: &str, hash: &str) -> Result<bool> {
-    let hash = PasswordHash::new(hash)?;
-    let hasher = Argon2::default();
-
-    Ok(hasher.verify_password(password.as_bytes(), &hash).is_ok())
-}
 
 pub struct RepoRepository<'a> {
     name: &'a str,
@@ -191,11 +24,11 @@ impl<'a> RepoRepository<'a> {
         Self::from_base(&DIRS.data_dir().join(user), repo)
     }
 
-    fn from_user_repo(user_repo: &UserRepository<'_>, name: &'a str) -> Self {
+    pub(super) fn from_user_repo(user_repo: &UserRepository<'_>, name: &'a str) -> Self {
         Self::from_base(&user_repo.user_path, name)
     }
 
-    fn from_base(base: &Utf8Path, name: &'a str) -> Self {
+    pub(super) fn from_base(base: &Utf8Path, name: &'a str) -> Self {
         let repo_path = base.join(name);
         let repo_file = repo_path.join("repo.json");
         let repo_git = Arc::new(repo_path.join("repo.git"));

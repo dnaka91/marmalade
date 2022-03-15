c496e8556794e84bbae28cf86c1fcfc7b9cfde7a
Dominik Nakamura <dnaka91@gmail.com>
2022-03-15 17:58:10 +0900

    Use same asset logic for favicons

--- build.rs
+++ build.rs
@@ -20,6 +20,7 @@ fn main() {
 
     let (webfonts, base_route) = render_webfonts(&root);
     let main_css = render_main_css(&root, &out, base_route);
+    let favicons = render_favicons(&root);
 
     let syntax = quote! {
         use headers::ETag;
@@ -27,6 +28,7 @@ fn main() {
 
         #main_css
         #webfonts
+        #favicons
     };
 
     fs::write(out.join("assets.rs"), syntax.to_string()).unwrap();
@@ -80,7 +82,7 @@ fn render_main_css(root: &str, out: &Path, webfonts_route: String) -> TokenStrea
 
     quote! {
         pub const MAIN_CSS_ROUTE: &str = #route;
-        pub static MAIN_CSS_CONTENT: &str = include_str!(concat!(env!("OUT_DIR"), "/main.css"));
+        pub static MAIN_CSS_CONTENT: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/main.css"));
         pub static MAIN_CSS_HASH: Lazy<ETag> =Lazy::new(|| #etag.parse().unwrap());
     }
 }
@@ -131,4 +133,33 @@ fn render_webfonts(root: &str) -> (TokenStream, String) {
     };
 
     (syntax, base_route)
+}
+
+fn render_favicons(root: &str) -> TokenStream {
+    let create_hash = |name| {
+        let path = format!("{root}/assets/{name}.png");
+        let content = fs::read(&path).unwrap();
+        let hash = {
+            let mut hasher = DefaultHasher::new();
+            hasher.write(&content);
+            hasher.finish()
+        };
+        let route = format!("/{name}-{hash:016x}.png");
+        let etag = format!("W/\"{hash:016x}\"");
+
+        (route, etag, path)
+    };
+
+    let (x16_route, x16_etag, x16_path) = create_hash("favicon-16x16");
+    let (x32_route, x32_etag, x32_path) = create_hash("favicon-32x32");
+
+    quote! {
+        pub const FAVICON_X16_ROUTE: &str = #x16_route;
+        pub static FAVICON_X16_CONTENT: &[u8] = include_bytes!(#x16_path);
+        pub static FAVICON_X16_HASH: Lazy<ETag> =Lazy::new(|| #x16_etag.parse().unwrap());
+
+        pub const FAVICON_X32_ROUTE: &str = #x32_route;
+        pub static FAVICON_X32_CONTENT: &[u8] = include_bytes!(#x32_path);
+        pub static FAVICON_X32_HASH: Lazy<ETag> =Lazy::new(|| #x32_etag.parse().unwrap());
+    }
 }
--- src/handlers/assets.rs
+++ src/handlers/assets.rs
@@ -8,56 +8,66 @@ use axum::{
     response::IntoResponse,
 };
 use headers::{
-    CacheControl, ContentType, HeaderMap, HeaderMapExt, IfModifiedSince, IfNoneMatch, LastModified,
+    CacheControl, ContentType, ETag, HeaderMap, HeaderMapExt, IfModifiedSince, IfNoneMatch,
+    LastModified,
 };
+use mime::Mime;
 use tracing::info;
 
 use crate::assets;
 
+type AssetResponse = Result<(HeaderMap, &'static [u8]), (HeaderMap, StatusCode)>;
+
 /// Max age setting for the `Cache-Control` header, currently set to **2 years**.
 const CACHE_MAX_AGE: Duration = Duration::from_secs(63_072_000);
 
-pub async fn favicon_32() -> impl IntoResponse {
+pub async fn favicon_32(
+    if_modified_since: Option<TypedHeader<IfModifiedSince>>,
+    if_none_match: Option<TypedHeader<IfNoneMatch>>,
+) -> AssetResponse {
     info!("got assets favicon-32 request");
-    include_bytes!("../../assets/favicon-32x32.png").as_ref()
+    favicon(
+        &*assets::FAVICON_X32_HASH,
+        assets::FAVICON_X32_CONTENT,
+        mime::IMAGE_PNG,
+        if_modified_since,
+        if_none_match,
+    )
 }
 
-pub async fn favicon_16() -> impl IntoResponse {
+pub async fn favicon_16(
+    if_modified_since: Option<TypedHeader<IfModifiedSince>>,
+    if_none_match: Option<TypedHeader<IfNoneMatch>>,
+) -> AssetResponse {
     info!("got assets favicon-16 request");
-    include_bytes!("../../assets/favicon-16x16.png").as_ref()
+    favicon(
+        &*assets::FAVICON_X16_HASH,
+        assets::FAVICON_X16_CONTENT,
+        mime::IMAGE_PNG,
+        if_modified_since,
+        if_none_match,
+    )
 }
 
 pub async fn main_css(
     if_modified_since: Option<TypedHeader<IfModifiedSince>>,
     if_none_match: Option<TypedHeader<IfNoneMatch>>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    let modified = if_modified_since.map(|v| v.is_modified(SystemTime::UNIX_EPOCH));
-    let unmatched = if_none_match.map(|v| v.precondition_passes(&*assets::MAIN_CSS_HASH));
-
-    info!(?modified, ?unmatched, "got assets main-css request");
-
-    let mut headers = HeaderMap::with_capacity(4);
-    headers.typed_insert(ContentType::from(mime::TEXT_CSS));
-    headers.typed_insert(
-        CacheControl::new()
-            .with_public()
-            .with_max_age(CACHE_MAX_AGE),
-    );
-    headers.typed_insert(assets::MAIN_CSS_HASH.clone());
-    headers.typed_insert(LastModified::from(SystemTime::UNIX_EPOCH));
-
-    if modified.or(unmatched).unwrap_or(true) {
-        Ok((headers, assets::MAIN_CSS_CONTENT))
-    } else {
-        Err((headers, StatusCode::NOT_MODIFIED))
-    }
+) -> impl IntoResponse {
+    info!("got assets main-css request");
+    favicon(
+        &*assets::MAIN_CSS_HASH,
+        assets::MAIN_CSS_CONTENT,
+        mime::TEXT_CSS,
+        if_modified_since,
+        if_none_match,
+    )
 }
 
 pub async fn webfonts(
     Path(path): Path<String>,
     if_modified_since: Option<TypedHeader<IfModifiedSince>>,
     if_none_match: Option<TypedHeader<IfNoneMatch>>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
+) -> AssetResponse {
     info!("got assets webfonts request");
 
     let index = assets::WEBFONTS_NAME
@@ -65,21 +75,37 @@ pub async fn webfonts(
         .position(|&route| route == path)
         .ok_or_else(|| (HeaderMap::new(), StatusCode::NOT_FOUND))?;
 
+    favicon(
+        &assets::WEBFONTS_HASH[index],
+        assets::WEBFONTS_CONTENT[index],
+        mime::FONT_WOFF2,
+        if_modified_since,
+        if_none_match,
+    )
+}
+
+fn favicon(
+    etag: &ETag,
+    content: &'static [u8],
+    mime: Mime,
+    if_modified_since: Option<TypedHeader<IfModifiedSince>>,
+    if_none_match: Option<TypedHeader<IfNoneMatch>>,
+) -> AssetResponse {
     let modified = if_modified_since.map(|v| v.is_modified(SystemTime::UNIX_EPOCH));
-    let unmatched = if_none_match.map(|v| v.precondition_passes(&assets::WEBFONTS_HASH[index]));
+    let unmatched = if_none_match.map(|v| v.precondition_passes(etag));
 
     let mut headers = HeaderMap::with_capacity(4);
-    headers.typed_insert(ContentType::from(mime::FONT_WOFF2));
+    headers.typed_insert(ContentType::from(mime));
     headers.typed_insert(
         CacheControl::new()
             .with_public()
             .with_max_age(CACHE_MAX_AGE),
     );
-    headers.typed_insert(assets::WEBFONTS_HASH[index].clone());
+    headers.typed_insert(etag.clone());
     headers.typed_insert(LastModified::from(SystemTime::UNIX_EPOCH));
 
     if modified.or(unmatched).unwrap_or(true) {
-        Ok((headers, assets::WEBFONTS_CONTENT[index]))
+        Ok((headers, content))
     } else {
         Err((headers, StatusCode::NOT_MODIFIED))
     }
